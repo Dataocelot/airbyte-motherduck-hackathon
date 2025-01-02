77d149adeea1c792291bdefd103334c3ac41182d
Eniwoke Cornelius <eniwokepdm@gmail.com>
2025-01-02 18:44:18 +0000

    feat: add logger and update requirements.txt

--- logger.py
+++ logger.py
@@ -3,8 +3,9 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
+
 class Logger:
-    def __init__(self, log_directory="logs"):
+    def __init__(self, log_directory=".logs"):
         if not os.path.exists(log_directory):
             os.makedirs(log_directory)
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -12,8 +13,8 @@ class Logger:
         self.logger = logging.getLogger("ApplianceScraperLogger")
         self.logger.setLevel(logging.INFO)
 
-        handler = RotatingFileHandler(log_filename, maxBytes=5*1024, backupCount=5)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler = RotatingFileHandler(log_filename, maxBytes=50 * 1024, backupCount=5)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
 
         self.logger.addHandler(handler)
