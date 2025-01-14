1bdd46521c75b6cca4fed13a1f802983f5b00547
Eniwoke Cornelius <eniwokepdm@gmail.com>
2025-01-14 21:21:16 +0000

    Add terraform

--- iac/main.tf
+++ iac/main.tf
@@ -4,19 +4,84 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+    airbyte = {
+      source = "airbytehq/airbyte"
+      version = "0.6.5"
+    }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
 }
+provider "docker" {}
 
+provider "airbyte" {
+  client_id = var.client_id
+  client_secret = var.client_secret
+}
+resource "random_id" "airbyte_id" {
+  byte_length = 8
+}
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
-    tags = {
-      Name        = var.bucket_name
-      Environment = "Production"
-      Purpose = "Hackathon"
-    }
+  tags = {
+    Name        = var.bucket_name
+    Environment = "dev"
+    Purpose     = "Hackathon"
+  }
+}
+
+resource "docker_image" "airbyte_hackathon" {
+  name = "airbyte_motherduck_hackathon:latest"
+
+  build {
+    context    = ".."
+    dockerfile = "../Dockerfile.app"
+  }
+  depends_on = [aws_s3_bucket.s3_bucket]
+}
+
+
+resource "docker_container" "airbyte-hackathon" {
+  name  = "airbyte-motherduck-container"
+  image = docker_image.airbyte_hackathon.name
+  ports {
+    internal = 8501
+    external = 8501
+  }
+  rm = true
+
+}
+
+resource "airbyte_source_s3" "aribyte_source_s3" {
+  configuration = {
+    aws_access_key_id     = "${var.aws_access_key_id}"
+    aws_secret_access_key = "${var.aws_secret_access_key}"
+    bucket                = aws_s3_bucket.s3_bucket.id
+    region_name           = var.aws_region
+    streams = [
+      {
+        days_to_sync_if_history_is_full = 3
+        format = {
+          jsonl_format = {
+            double_as_string = true
+          }
+        }
+        globs = [
+          "output/brand=*/model_number=*/sections/*.json",
+        ]
+        name                                        = "manual_sections"
+        recent_n_files_to_read_for_schema_discovery = 3
+        schemaless                                  = true
+        validation_policy                           = "Emit Record"
+      },
+    ]
+  }
+  name          = "Airbyte-motherduck-s3-${random_id.airbyte_id.hex}"
+  workspace_id  = var.airbyte_workspace_id
 }
--- iac/variable.tf
+++ iac/variable.tf
@@ -1,16 +1,38 @@
 variable "bucket_name" {
-   type = string
-   default = "airbyte-motherduck-hackathon-1"
-   description = "Default name of the bucket for this project"
+  type        = string
+  default     = "airbyte-motherduck-hackathon-1"
+  description = "Default name of the bucket for this project"
 }
 
 variable "aws_region" {
-   type = string
-   default = "eu-west-2"
-   description = "The region for the bucket"
+  type        = string
+  default     = "eu-west-2"
+  description = "The region for the bucket"
 }
 
+variable "aws_secret_access_key" {
+  type        = string
+  description = "The AWS secret key"
+}
+
+
+variable "aws_access_key_id" {
+  type        = string
+  description = "The AWS secret key id"
+}
+
+
 variable "airbyte_workspace_id" {
-   type = string
-   description = "Your Airbyte workspace ID"
+  type        = string
+  description = "The Airbyte workspace ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "The airbyte client id"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "The airbyte client secret"
 }
